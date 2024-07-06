c96227e4361fd0ec900a4ce880a1368e9431b2f4
Akbar Odilov <akbarjonodilov01@gmail.com>
2024-07-06 11:26:32 +0500

    11:26 | 07/06/2024

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-tuning house cdn
+tuning house cdn<br/>
+https://documenter.getpostman.com/view/24661639/2sA3e1AUuU
--- src/middlewares/multer.config.js
+++ src/middlewares/multer.config.js
@@ -15,5 +15,4 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-
 module.exports = upload;
